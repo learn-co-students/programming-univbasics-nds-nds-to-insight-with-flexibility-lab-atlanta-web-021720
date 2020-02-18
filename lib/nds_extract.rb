8cd22001501481bdcd487620aaf1ed069409ee79
SpencerBlum <spencerblum@gmail.com>
2020-02-18 17:35:00 -0500

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -167,7 +167,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-end
+
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
